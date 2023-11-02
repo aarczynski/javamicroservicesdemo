8cee6f0b412e167e7942b767469a5b2016c35efc
aarczynski <adam.arczynski@gmail.com>
2023-11-02 11:25:16 +0100

    improve sql with sample data

--- company/src/main/resources/db/migration/postgres/V1_1__demo-data.sql
+++ company/src/main/resources/db/migration/postgres/V1_1__demo-data.sql
@@ -13,11 +13,11 @@ VALUES ('Joe', 'Doe', 'joe.doe@company.com', 10000.00, 'ACTIVE'),
        ('Jan', 'Kowalski', 'jan.kowalski@company.com', 10000.00, 'ACTIVE');
 
 INSERT INTO department_employee(department_id, employee_id)
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'IT' AND e.email = 'joe.doe@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Finances' AND e.email = 'joe.doe@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Finances' AND e.email = 'jan.nowak@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Delivery' AND e.email = 'jan.kowalski@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Delivery' AND e.email = 'anna.wesola@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Delivery' AND e.email = 'ilona.iksinska@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Ops' AND e.email = 'jan.nowak@company.com' UNION
-SELECT d.id, e.id FROM department d, employee e WHERE d.name = 'Ops' AND e.email = 'jan.kowalski@company.com';
+SELECT (SELECT id FROM department WHERE name = 'IT'), (SELECT id FROM employee WHERE email = 'joe.doe@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Finances'), (SELECT id FROM employee WHERE email = 'joe.doe@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Finances'), (SELECT id FROM employee WHERE email = 'jan.nowak@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Delivery'), (SELECT id FROM employee WHERE email = 'jan.kowalski@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Delivery'), (SELECT id FROM employee WHERE email = 'anna.wesola@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Delivery'), (SELECT id FROM employee WHERE email = 'ilona.iksinska@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Ops'), (SELECT id FROM employee WHERE email = 'jan.nowak@company.com') UNION
+SELECT (SELECT id FROM department WHERE name = 'Ops'), (SELECT id FROM employee WHERE email = 'jan.kowalski@company.com');
