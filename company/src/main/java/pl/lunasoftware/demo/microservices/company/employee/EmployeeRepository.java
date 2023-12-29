b676ef5d17499077bd62e90fd4652bfd6cec413e
aarczynski <adam.arczynski@gmail.com>
2023-12-29 18:11:42 +0100

    db monitoring

--- company/src/main/java/pl/lunasoftware/demo/microservices/company/employee/EmployeeRepository.java
+++ company/src/main/java/pl/lunasoftware/demo/microservices/company/employee/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package pl.lunasoftware.demo.microservices.company.employee;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, UUID> {
 
+    @Timed(value = "db_employee", histogram = true, percentiles = {0.5, 0.75, 0.9, 0.95, 0.99})
     @EntityGraph(value = "employee-departments")
     Optional<EmployeeEntity> findEmployeeByEmail(String email);
 }
