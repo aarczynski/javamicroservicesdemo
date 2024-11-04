90746056ef3289b3a370c650011c86bdd174e647
aarczynski <adam.arczynski@gmail.com>
2024-11-04 12:39:25 +0100

    Revert "added spans for client microservice"
    This reverts commit 3d02a59cffb6eec14845c336b9b50f4304399490.

--- app-company-client/src/main/java/pl/lunasoftware/demo/microservices/company/client/CompanyClient.java
+++ app-company-client/src/main/java/pl/lunasoftware/demo/microservices/company/client/CompanyClient.java
@@ -1,6 +1,5 @@
 package pl.lunasoftware.demo.microservices.company.client;
 
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "CompanyAppClient", path = "/api/v1")
 public interface CompanyClient {
 
-    @WithSpan
     @GetMapping("/employees/{email}")
     EmployeeDto getEmployeeByEmail(@PathVariable String email);
 
-    @WithSpan
     @GetMapping("/departments/{departmentName}/costs")
     DepartmentCostDto getDepartmentCost(@PathVariable String departmentName);
 
