3d02a59cffb6eec14845c336b9b50f4304399490
aarczynski <adam.arczynski@gmail.com>
2024-11-04 12:33:47 +0100

    added spans for client microservice

--- app-company-client/src/main/java/pl/lunasoftware/demo/microservices/company/client/CompanyClient.java
+++ app-company-client/src/main/java/pl/lunasoftware/demo/microservices/company/client/CompanyClient.java
@@ -1,5 +1,6 @@
 package pl.lunasoftware.demo.microservices.company.client;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "CompanyAppClient", path = "/api/v1")
 public interface CompanyClient {
 
+    @WithSpan
     @GetMapping("/employees/{email}")
     EmployeeDto getEmployeeByEmail(@PathVariable String email);
 
+    @WithSpan
     @GetMapping("/departments/{departmentName}/costs")
     DepartmentCostDto getDepartmentCost(@PathVariable String departmentName);
 
