0cb0e41242402ef20253adc9c47b851033e24eb2
aarczynski <adam.arczynski@gmail.com>
2024-12-10 10:37:46 +0100

    rest template test

--- app-company-client/src/main/java/pl/lunasoftware/demo/microservices/company/client/CompanyClientController.java
+++ app-company-client/src/main/java/pl/lunasoftware/demo/microservices/company/client/CompanyClientController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -25,6 +26,7 @@ public class CompanyClientController {
     private final Executor executor = Executors.newFixedThreadPool(4);
 
     private final CompanyClient companyClient;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public CompanyClientController(CompanyClient companyClient) {
         this.companyClient = companyClient;
@@ -33,7 +35,10 @@ public class CompanyClientController {
     @GetMapping("/employees/{email}")
     public EmployeeDto getEmployeeByEmail(@PathVariable String email) throws ExecutionException, InterruptedException {
         LOG.info("Received request for employee with email {}, forwarding to Company App", email);
-        EmployeeDto employee = CompletableFuture.supplyAsync(() -> companyClient.getEmployeeByEmail(email), executor).get();
+//        EmployeeDto employee = CompletableFuture.supplyAsync(() -> companyClient.getEmployeeByEmail(email), executor).get();
+        EmployeeDto employee = CompletableFuture.supplyAsync(
+                () -> restTemplate.getForEntity("http://app-company:8080/api/v1/employees/" + email, EmployeeDto.class), executor
+        ).get().getBody();
         LOG.info("Received response from Company App for employee with email {}", email);
         return employee;
     }
