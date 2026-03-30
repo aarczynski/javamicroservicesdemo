1861c7020c834e4a57b2b94b7de61ecccdab7487
Adam Arczynski <adam.arczynski@gmail.com>
2026-03-30 20:42:37 +0200

    observed

--- app-job-offers/src/main/java/pl/lunasoftware/demo/microservices/joboffers/skill/SkillRepository.java
+++ app-job-offers/src/main/java/pl/lunasoftware/demo/microservices/joboffers/skill/SkillRepository.java
@@ -1,5 +1,6 @@
 package pl.lunasoftware.demo.microservices.joboffers.skill;
 
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Observed(name = "job-offers.db")
 @Repository
 public interface SkillRepository extends JpaRepository<SkillEntity, UUID> {
 
