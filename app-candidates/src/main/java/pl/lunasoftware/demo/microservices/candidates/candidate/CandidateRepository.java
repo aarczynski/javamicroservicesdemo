f278d4ee42aa42897a5edf8fcfd8fc5b993ac268
Adam Arczynski <adam.arczynski@gmail.com>
2026-04-04 23:39:48 +0200

    update monitoring dashboard

--- app-candidates/src/main/java/pl/lunasoftware/demo/microservices/candidates/candidate/CandidateRepository.java
+++ app-candidates/src/main/java/pl/lunasoftware/demo/microservices/candidates/candidate/CandidateRepository.java
@@ -1,6 +1,5 @@
 package pl.lunasoftware.demo.microservices.candidates.candidate;
 
-import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Observed(name = "candidates.db")
 @Repository
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
 
--- app-job-offers/src/main/java/pl/lunasoftware/demo/microservices/joboffers/offer/JobOfferRepository.java
+++ app-job-offers/src/main/java/pl/lunasoftware/demo/microservices/joboffers/offer/JobOfferRepository.java
@@ -1,6 +1,5 @@
 package pl.lunasoftware.demo.microservices.joboffers.offer;
 
-import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +10,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-@Observed(name = "job-offers.db")
 @Repository
 public interface JobOfferRepository extends JpaRepository<JobOfferEntity, UUID> {
 
